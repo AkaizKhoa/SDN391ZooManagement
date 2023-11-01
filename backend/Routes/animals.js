3231e0e9585eca6c5e95c538d1a680419f2e210a
Trương Đinh Đăng Khoa <khoatdd0811@gmail.com>
2023-11-01 17:06:47 +0700

    animals create and update cage

--- backend/Routes/animals.js
+++ backend/Routes/animals.js
@@ -6,94 +6,108 @@ const express = require('express');
 
 //Import The Created User Model
 
-const Posts = require('../models/animal');
-
+const Animals = require('../models/animal');
 //Invoke express.Router() To Send Requests To routes And Write http Requests
+
+const Cages = require('../models/Cage')
 
 const router = express.Router();
 
-//Save Posts
+//Save Animals
 
-router.post('/animal/save',(req,res)=>{
+router.post('/animal/save', (req, res) => {
 
-//Instantiate Posts Via A Constructor
+    //Instantiate Animals Via A Constructor
 
-let newPost = new Posts(req.body);
+    let newAnimal = new Animals(req.body);
 
-newPost.save((err)=>{
-    if(err){
-        return res.status(400).json({
-            error:err
-        });
-    }
-        return res.status(200).json({
-            success:"Posts Saved Successfully!"
-        });
-});
+
+
+    let cageId = req.body.cage;
+    Cages.findByIdAndUpdate(
+        cageId,
+        { $set: { isEmpty: true } }, // Đặt lại isEmpty thành true
+        (err, cage) => {
+            if (err) {
+                return res.status(400).json({ error: err });
+            } else {
+                // Sau khi cập nhật isEmpty thành true, bạn có thể thêm newAnimal
+                newAnimal.save((err) => {
+                    if (err) {
+                        return res.status(400).json({ error: err });
+                    } else {
+                        return res.status(200).json({
+                            success: "Updated isEmpty and Create new Animal Successfully!"
+                        });
+                    }
+                });
+            }
+        }
+    );
 
 });
 
 //Get A Specific Post
 
-router.get("/animal/:id",(req,res) => {
-    let postId  = req.params.id;
-    Posts.findById(postId,(err,post) => {
-        if(err){
+router.get("/animal/:id", (req, res) => {
+    let postId = req.params.id;
+    Animals.findById(postId, (err, post) => {
+        if (err) {
             return res.status(400).json({
-                success:false,err
+                success: false, err
             });
         }
         return res.status(200).json({
-            success:true,
+            success: true,
             post
         });
     });
 });
 
-//Get Posts
+//Get Animals
 
-router.get('/animal',(req,res)=>{
-    Posts.find().exec((err,posts)=>{
-        if(err){
+router.get('/animal', (req, res) => {
+    Animals.find().exec((err, animals) => {
+        if (err) {
             return res.status(400).json({
-                error:err
+                error: err
             });
         }
         return res.status(200).json({
-            success:true,
-            existingPosts:posts
+            success: true,
+            existingAnimals: animals
         });
     });
 });
 
-//Update Posts
+//Update Animals
 
-router.put('/animal/update/:id',(req,res)=>{
-    Posts.findByIdAndUpdate(
+router.put('/animal/update/:id', (req, res) => {
+    Animals.findByIdAndUpdate(
         req.params.id,
         {
-            $set:req.body
+            $set: req.body
         },
-        (err,post)=>{
-            if(err){
-                return res.status(400).json({error:err});
+        (err, post) => {
+            if (err) {
+                return res.status(400).json({ error: err });
             }
             return res.status(200).json({
-                success:"Updated Successfully!"
+                success: "Updated Successfully!"
             });
         }
     );
 });
 
-//Delete Posts
+//Delete Animals
 
-router.delete('/animal/delete/:id',(req,res)=>{
-    Posts.findByIdAndRemove(req.params.id).exec((err,deletedPost)=>{
-        if(err) return res.status(400).json({
-            message:"Delete Unsuccesfull!",err
+router.delete('/animal/delete/:id', (req, res) => {
+    Animals.findByIdAndRemove(req.params.id).exec((err, deletedPost) => {
+        if (err) return res.status(400).json({
+            message: "Delete Unsuccesfull!", err
         });
         return res.json({
-            message:"Deleted Successfully!",deletedPost
+            message: "Deleted Successfully!", deletedPost
         });
     });
 });
