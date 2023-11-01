498ce01776c2a49cc3e8b2ea72c0339162d35290
holeshit1234 <hominhdund@gmail.com>
2023-11-01 22:43:33 +0700

    zoo trainer get all cage by trainer id and animal by id cage

--- backend/Routes/animals.js
+++ backend/Routes/animals.js
@@ -2,114 +2,136 @@
 
 //Import Express
 
-const express = require('express');
+const express = require("express");
 
 //Import The Created User Model
 
-const Animals = require('../models/animal');
+const Animals = require("../models/animal");
 //Invoke express.Router() To Send Requests To routes And Write http Requests
 
-const Cages = require('../models/Cage')
+const Cages = require("../models/Cage");
 
 const router = express.Router();
 
 //Save Animals
 
-router.post('/animal/save', (req, res) => {
+router.post("/animal/save", (req, res) => {
+  //Instantiate Animals Via A Constructor
 
-    //Instantiate Animals Via A Constructor
+  let newAnimal = new Animals(req.body);
 
-    let newAnimal = new Animals(req.body);
-
-
-
-    let cageId = req.body.cage;
-    Cages.findByIdAndUpdate(
-        cageId,
-        { $set: { isEmpty: true } }, // Đặt lại isEmpty thành true
-        (err, cage) => {
-            if (err) {
-                return res.status(400).json({ error: err });
-            } else {
-                // Sau khi cập nhật isEmpty thành true, bạn có thể thêm newAnimal
-                newAnimal.save((err) => {
-                    if (err) {
-                        return res.status(400).json({ error: err });
-                    } else {
-                        return res.status(200).json({
-                            success: "Updated isEmpty and Create new Animal Successfully!"
-                        });
-                    }
-                });
-            }
-        }
-    );
-
+  let cageId = req.body.cage;
+  Cages.findByIdAndUpdate(
+    cageId,
+    { $set: { isEmpty: true } }, // Đặt lại isEmpty thành true
+    (err, cage) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      } else {
+        // Sau khi cập nhật isEmpty thành true, bạn có thể thêm newAnimal
+        newAnimal.save((err) => {
+          if (err) {
+            return res.status(400).json({ error: err });
+          } else {
+            return res.status(200).json({
+              success: "Updated isEmpty and Create new Animal Successfully!",
+            });
+          }
+        });
+      }
+    }
+  );
 });
 
 //Get A Specific Post
 
 router.get("/animal/:id", (req, res) => {
-    let postId = req.params.id;
-    Animals.findById(postId, (err, post) => {
-        if (err) {
-            return res.status(400).json({
-                success: false, err
-            });
-        }
-        return res.status(200).json({
-            success: true,
-            post
-        });
+  let postId = req.params.id;
+  Animals.findById(postId, (err, post) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      post,
     });
+  });
 });
 
 //Get Animals
 
-router.get('/animal', (req, res) => {
-    Animals.find().exec((err, animals) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            });
-        }
-        return res.status(200).json({
-            success: true,
-            existingAnimals: animals
-        });
+router.get("/animal", (req, res) => {
+  Animals.find().exec((err, animals) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingAnimals: animals,
     });
+  });
 });
 
 //Update Animals
 
-router.put('/animal/update/:id', (req, res) => {
-    Animals.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: req.body
-        },
-        (err, post) => {
-            if (err) {
-                return res.status(400).json({ error: err });
-            }
-            return res.status(200).json({
-                success: "Updated Successfully!"
-            });
-        }
-    );
+router.put("/animal/update/:id", (req, res) => {
+  Animals.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, post) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      return res.status(200).json({
+        success: "Updated Successfully!",
+      });
+    }
+  );
 });
 
 //Delete Animals
 
-router.delete('/animal/delete/:id', (req, res) => {
-    Animals.findByIdAndRemove(req.params.id).exec((err, deletedPost) => {
-        if (err) return res.status(400).json({
-            message: "Delete Unsuccesfull!", err
-        });
-        return res.json({
-            message: "Deleted Successfully!", deletedPost
-        });
+router.delete("/animal/delete/:id", (req, res) => {
+  Animals.findByIdAndRemove(req.params.id).exec((err, deletedPost) => {
+    if (err)
+      return res.status(400).json({
+        message: "Delete Unsuccesfull!",
+        err,
+      });
+    return res.json({
+      message: "Deleted Successfully!",
+      deletedPost,
     });
+  });
+});
+
+// get detail animal in the cage
+
+router.get("/animal/cage/:id", (req, res) => {
+  let cageId = req.params.id;
+  Animals.find({ cage: cageId }, (err, animal) => {
+    if (err) {
+      res.status(500).json({
+        message: "Error finding animal",
+      });
+    } else if (animal.length === 0) {
+      res.status(404).json({
+        message: "No animal found for this user",
+      });
+    } else {
+      res.status(200).json({
+        message: "Success",
+        animal: animal,
+      });
+    }
+  });
 });
 
 //Export
--- backend/Routes/cage.js
+++ backend/Routes/cage.js
@@ -2,11 +2,11 @@
 
 //Import Express
 
-const express = require('express');
+const express = require("express");
 
 //Import The Created User Model
 
-const Cages = require('../models/Cage');
+const Cages = require("../models/Cage");
 
 //Invoke express.Router() To Send Requests To routes And Write http Requests
 
@@ -14,89 +14,113 @@ const router = express.Router();
 
 //Save Cages
 
-router.post('/cage/save', (req, res) => {
+router.post("/cage/save", (req, res) => {
+  //Instantiate Cages Via A Constructor
 
-    //Instantiate Cages Via A Constructor
+  let newCage = new Cages(req.body);
 
-    let newCage = new Cages(req.body);
-
-    newCage.save((err) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            });
-        }
-        return res.status(200).json({
-            success: "Cages Saved Successfully!"
-        });
+  newCage.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: "Cages Saved Successfully!",
     });
-
+  });
 });
 
 //Get A Specific Cage
 
 router.get("/cage/:id", (req, res) => {
-    let cageId = req.params.id;
-    console.log(cageId);
-    Cages.findById(cageId, (err, cage) => {
-        if (err) {
-            return res.status(400).json({
-                success: false, err
-            });
-        }
-        return res.status(200).json({
-            success: true,
-            cage
-        });
+  let cageId = req.params.id;
+  console.log(cageId);
+  Cages.findById(cageId, (err, cage) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      cage,
     });
+  });
 });
 
 //Get Cages
 
-router.get('/cage', (req, res) => {
-    Cages.find().exec((err, cages) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            });
-        }
-        return res.status(200).json({
-            success: true,
-            existingCages: cages
-        });
+router.get("/cage", (req, res) => {
+  Cages.find().exec((err, cages) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingCages: cages,
     });
+  });
 });
 
 //Update Cages
 
-router.put('/cage/update/:id',(req,res)=>{
-    Cages.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set:req.body
-        },
-        (err,cage)=>{
-            if(err){
-                return res.status(400).json({error:err});
-            }
-            return res.status(200).json({
-                success:"Updated Successfully!"
-            });
-        }
-    );
+router.put("/cage/update/:id", (req, res) => {
+  Cages.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, cage) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
+      return res.status(200).json({
+        success: "Updated Successfully!",
+      });
+    }
+  );
 });
 
 //Delete Cages
 
-router.delete('/cage/delete/:id',(req,res)=>{
-    Cages.findByIdAndRemove(req.params.id).exec((err,deletedCage)=>{
-        if(err) return res.status(400).json({
-            message:"Delete Unsuccesfull!",err
-        });
-        return res.json({
-            message:"Deleted Successfully!",deletedCage
-        });
+router.delete("/cage/delete/:id", (req, res) => {
+  Cages.findByIdAndRemove(req.params.id).exec((err, deletedCage) => {
+    if (err)
+      return res.status(400).json({
+        message: "Delete Unsuccesfull!",
+        err,
+      });
+    return res.json({
+      message: "Deleted Successfully!",
+      deletedCage,
     });
+  });
+});
+
+// GET CAGE BY ZOO TRAINER
+
+router.get("/cage/trainer/:id", (req, res) => {
+  let userID = req.params.id;
+  Cages.find({ user: userID }, (err, cages) => {
+    if (err) {
+      res.status(500).json({
+        message: "Error finding cages",
+      });
+    } else if (cages.length === 0) {
+      res.status(404).json({
+        message: "No cages found for this user",
+      });
+    } else {
+      res.status(200).json({
+        message: "Success",
+        cages: cages,
+      });
+    }
+  });
 });
 
 //Export
